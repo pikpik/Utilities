e9e86575754a703eafe3e8fe83dbdb04c5416798
pikpik <pikpik.1010@gmail.com>
2012-09-27 21:51:46 -0400

    I added a script for checking for updates.

---
+++ maintenance/updates.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+src=/usr/src
+
+
+if [ "X$1" != "X" ]
+
+then src="$1"
+
+fi
+
+
+(
+
+	cd $src
+
+	git fetch origin
+
+	git log \
+		origin/master...master \
+		--pretty=format:"%cr - %s (%h)" \
+	| awk '
+		BEGIN {
+			print ""
+		}
+
+		{
+			print $0
+		}
+
+		END {
+			print ""
+			print "\t" NR ( NR == 1 ? " update" : " updates" )
+			print ""
+			print "To update: git pull"
+			print ""
+		}
+	'
+
+)
